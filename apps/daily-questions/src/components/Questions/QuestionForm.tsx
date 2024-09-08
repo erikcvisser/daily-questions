7bf7273579f75d64fd331ffe858e86f08a04e338
Erik Visser <erikcvisser@gmail.com>
2024-09-08 19:14:19 +0200

    remove comments from questionform

--- apps/daily-questions/src/components/Questions/QuestionForm.tsx
+++ apps/daily-questions/src/components/Questions/QuestionForm.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Button, Group, NumberInput, Select, TextInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { zodResolver } from 'mantine-form-zod-resolver';
@@ -11,21 +11,8 @@ import { useFocusTrap } from '@mantine/hooks';
 import { notifications } from '@mantine/notifications';
 
 export default function QuestionForm({ question }: { question?: Question }) {
-  // const [title, setTitle] = useState('dummy string');
-  // const [type, setType] = useState<string | null>('INTEGER');
-  // const [targetInt, setTargetInt] = useState<string | number | null>();
-  // const [targetBool, setTargetBool] = useState<string | null>();
   const [submitting, setSubmitting] = useState(false);
   const focusTrapRef = useFocusTrap();
-
-  // useEffect(() => {
-  //   if (question) {
-  //     setTitle(question.title);
-  //     setType(question.type);
-  //     setTargetInt(question.targetInt || '');
-  //     setTargetBool(question.targetBool?.toString());
-  //   }
-  // }, [question]);
 
   const form = useForm({
     mode: 'uncontrolled',
@@ -49,14 +36,9 @@ export default function QuestionForm({ question }: { question?: Question }) {
     }
   };
 
-  // const updateQuestionWithId = question
-  //   ? updateQuestion.bind(null, question.id)
-  //   : undefined;
-
   const handleSubmit = (values: typeof form.values) => {
     setSubmitting(true);
     if (question) {
-      // const updateQuestionWithId = updateQuestion.bind(null, question.id);
       updateQuestion(question.id, values);
     } else {
       createQuestion(values);
@@ -66,13 +48,7 @@ export default function QuestionForm({ question }: { question?: Question }) {
     <>
       <form
         onSubmit={form.onSubmit(handleSubmit, handleError)}
-        // onSubmit={form.onSubmit(() => {
-        //   console.log('123');
-        // })}
-        //onSubmit={form.onSubmit(setSubmittedValues)}
-        //action={question ? updateQuestionWithId : createQuestion}
         className="flex gap-3 items-center"
-        //    onSubmit={() => setSubmitting(true)}
         ref={focusTrapRef}
       >
         <TextInput
@@ -81,10 +57,7 @@ export default function QuestionForm({ question }: { question?: Question }) {
           placeholder="Did I do my best to..."
           key={form.key('title')}
           {...form.getInputProps('title')}
-          //      name="title"
-          //      value={title}
           data-autofocus
-          //     onChange={(e) => setTitle(e.currentTarget.value)}
           withAsterisk
         />
         <Select
@@ -92,28 +65,22 @@ export default function QuestionForm({ question }: { question?: Question }) {
           placeholder="Pick value"
           key={form.key('type')}
           {...form.getInputProps('type')}
-          //      value={type}
-          // name="type"
-          //      onChange={(_value, option) => setType(_value)}
           comboboxProps={{ shadow: 'md' }}
           data={[
             { value: 'BOOLEAN', label: 'Yes/no' },
             { value: 'INTEGER', label: 'Numeric score (1-10)' },
             { value: 'FREETEXT', label: 'Free text' },
           ]}
-          required
+          withAsterisk
         />
 
         {form.getValues()['type'] == 'INTEGER' && (
           <NumberInput
             label="Target value (0-10)"
-            //name="targetInt"
             key={form.key('targetInt')}
             {...form.getInputProps('targetInt')}
             min={0}
             max={10}
-            //    value={parseInt(targetInt as string)}
-            //    onChange={setTargetInt}
             placeholder="Target value"
             withAsterisk
           />
@@ -122,17 +89,14 @@ export default function QuestionForm({ question }: { question?: Question }) {
           <Select
             label="Desired value"
             placeholder="Pick value"
-            // name="targetBool"
             key={form.key('targetBool')}
             {...form.getInputProps('targetBool')}
-            // value={targetBool}
-            // onChange={(_value, option) => setTargetBool(_value)}
             comboboxProps={{ shadow: 'md' }}
             data={[
               { value: 'true', label: 'Yes' },
               { value: 'false', label: 'No' },
             ]}
-            required
+            withAsterisk
           />
         )}
         <Group justify="flex-end" mt="md">
