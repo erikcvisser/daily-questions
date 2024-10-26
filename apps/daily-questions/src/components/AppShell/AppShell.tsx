adf1237fa983c1e74f37b5843d5bfc93309931c8
Erik Visser <erikcvisser@gmail.com>
2024-10-26 12:34:56 +0200

    add react swipable for menu

--- apps/daily-questions/src/components/AppShell/AppShell.tsx
+++ apps/daily-questions/src/components/AppShell/AppShell.tsx
@@ -20,6 +20,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Session } from 'next-auth';
 import { LoginLinkUnstyled } from '@/components/Header/AuthComponents';
+import { useSwipeable } from 'react-swipeable';
 
 export function BasicAppShell({
   children,
@@ -28,79 +29,94 @@ export function BasicAppShell({
   children: any;
   session: Session | undefined;
 }) {
-  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+  const [mobileOpened, { toggle: toggleMobile, close: closeMobile }] =
+    useDisclosure(false);
   const desktopOpened = session ? true : false;
 
+  // Define swipe handlers
+  const handlers = useSwipeable({
+    onSwipedRight: (eventData) => {
+      // Only open the navbar if it's not already open
+      if (!mobileOpened) {
+        toggleMobile();
+      }
+    },
+    delta: 50,
+    trackTouch: true,
+  });
+
   return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{
-        width: { base: 200, md: 250, lg: 300 },
-        breakpoint: 'sm',
-        collapsed: { desktop: !desktopOpened, mobile: !mobileOpened },
-      }}
-      padding="md"
-    >
-      <AppShellHeader px="md">
-        <Grid align="center" justify="space-between">
-          <GridCol span="content">
-            <Group>
-              <Burger
-                opened={mobileOpened}
-                onClick={toggleMobile}
-                hiddenFrom="sm"
-                size="sm"
-              />
-              <Link
-                href="/"
-                style={{
-                  display: 'inline-flex',
-                  textDecoration: 'none',
-                  alignItems: 'center',
-                  color: 'inherit',
-                  fontWeight: 'bold',
-                }}
-              >
-                <Image
-                  src="/android-chrome-192x192.png"
-                  width={32}
-                  height={32}
-                  alt={''}
+    <div {...handlers}>
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: { base: 200, md: 250, lg: 300 },
+          breakpoint: 'sm',
+          collapsed: { desktop: !desktopOpened, mobile: !mobileOpened },
+        }}
+        padding="md"
+      >
+        <AppShellHeader px="md">
+          <Grid align="center" justify="space-between">
+            <GridCol span="content">
+              <Group>
+                <Burger
+                  opened={mobileOpened}
+                  onClick={toggleMobile}
+                  hiddenFrom="sm"
+                  size="sm"
                 />
-                <Text p={16} size="xl" fw={'bold'}>
-                  Daily Questions
-                </Text>
-              </Link>
-            </Group>
-          </GridCol>
-          <GridCol span="content">
-            <Group>
-              <Group gap={64} visibleFrom="sm" mr={32}>
-                <UnstyledButton
-                  className={classes.control}
-                  component={Link}
+                <Link
                   href="/"
+                  style={{
+                    display: 'inline-flex',
+                    textDecoration: 'none',
+                    alignItems: 'center',
+                    color: 'inherit',
+                    fontWeight: 'bold',
+                  }}
                 >
-                  Home
-                </UnstyledButton>
-                <UnstyledButton
-                  className={classes.control}
-                  component={Link}
-                  href="/about"
-                >
-                  About
-                </UnstyledButton>
-                {!session && <LoginLinkUnstyled />}
+                  <Image
+                    src="/android-chrome-192x192.png"
+                    width={32}
+                    height={32}
+                    alt={''}
+                  />
+                  <Text p={16} size="xl" fw={'bold'}>
+                    Daily Questions
+                  </Text>
+                </Link>
               </Group>
-              <ColorSchemeToggle />
-            </Group>
-          </GridCol>
-        </Grid>
-      </AppShellHeader>
-      <AppShellNavbar p="md" zIndex={20}>
-        <Links session={session} toggleMobile={toggleMobile} />
-      </AppShellNavbar>
-      <AppShellMain>{children}</AppShellMain>
-    </AppShell>
+            </GridCol>
+            <GridCol span="content">
+              <Group>
+                <Group gap={64} visibleFrom="sm" mr={32}>
+                  <UnstyledButton
+                    className={classes.control}
+                    component={Link}
+                    href="/"
+                  >
+                    Home
+                  </UnstyledButton>
+                  <UnstyledButton
+                    className={classes.control}
+                    component={Link}
+                    href="/about"
+                  >
+                    About
+                  </UnstyledButton>
+                  {!session && <LoginLinkUnstyled />}
+                </Group>
+                <ColorSchemeToggle />
+              </Group>
+            </GridCol>
+          </Grid>
+        </AppShellHeader>
+        <AppShellNavbar p="md" zIndex={20}>
+          <Links session={session} toggleMobile={toggleMobile} />
+        </AppShellNavbar>
+        <AppShellMain>{children}</AppShellMain>
+      </AppShell>
+    </div>
   );
 }
