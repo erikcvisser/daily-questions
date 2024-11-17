6d31d1a190ccabcc47dd12baaa04b91a0f82de9f
Erik Visser <erikcvisser@gmail.com>
2024-11-17 13:39:30 +0100

    add headers on nextconfig

--- apps/daily-questions/next.config.js
+++ apps/daily-questions/next.config.js
@@ -45,6 +45,44 @@ module.exports = async (phase) => {
       removeConsole:
         process.env.NODE_ENV === 'production' ? { exclude: ['error'] } : false,
     },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'strict-origin-when-cross-origin',
+            },
+          ],
+        },
+        {
+          source: '/sw.js',
+          headers: [
+            {
+              key: 'Content-Type',
+              value: 'application/javascript; charset=utf-8',
+            },
+            {
+              key: 'Cache-Control',
+              value: 'no-cache, no-store, must-revalidate',
+            },
+            {
+              key: 'Content-Security-Policy',
+              value: "default-src 'self'; script-src 'self'",
+            },
+          ],
+        },
+      ];
+    },
   };
 
   if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
